3262b126ca63e6d590597d8be341c9c125f00446
Michael Walsh <michael.c.walsh1@gmail.com>
2016-10-17 16:46:07 -0400

    created users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    name "Michael C Walsh"
+    sequence(:username) { |n| "michaelcwalsh#{n}" }
+    sequence(:email) { |n| "michael#{n}@gmail.com" }
+    password "123456"
+    location "Boston, MA"
+    bio "Hello, World!"
   end
 end
