6265acb3bedd9599a098952fb4ba6a352f09543e
Michael Walsh <michael.c.walsh1@gmail.com>
2016-10-17 16:04:25 -0400

    added user views and updated the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [
+      :name,
+      :username,
+      :location,
+      :bio,
+      :email,
+      :avatar,
+      :password,
+      :password_confirmation,
+      :remember_me
+    ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+
 end
