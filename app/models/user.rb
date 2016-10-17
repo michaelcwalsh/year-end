70432384d53266d55d88f8ede934190f4a9aa383
Michael Walsh <michael.c.walsh1@gmail.com>
2016-10-17 14:07:22 -0400

    created user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+   validates :username, uniqueness: true, presence: true
+   validates :name, presence: true
+   validates :email, uniqueness: true, presence: true
+   validates :password, presence: true
 end
