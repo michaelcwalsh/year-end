684d53df0eda8c78bffe58b6742e3299787a3d93
Michael Walsh <michael.c.walsh1@gmail.com>
2016-10-19 11:24:46 -0400

    fixed typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
-  resources :movies
-
   get "/pages/:page" => "pages#show"
   root "pages#show", page: "home"
+
+  devise_for :users
+  resources :users
+  resources :albums
 end
