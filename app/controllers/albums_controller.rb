64f0ef888ac023114c8020dd3025f4deb95cb1f7
Michael Walsh <michael.c.walsh1@gmail.com>
2016-10-25 10:39:00 -0400

    cleaned up/edited albums controller and added show page

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,13 +1,31 @@
 class AlbumsController < ApplicationController
+  require 'httparty'
+  def show
+    @album = Album.new
+    @id = params[:id]
+    @result = HTTParty.get('https://api.spotify.com/v1/albums/' + @id)
+  end
   def new
     @album = Album.new
-    albums_json = { "title": @album.title, "artist": @album.artist }
-    respond_to do |format|
-      format.html
-      format.json { render json: albums_json }
-    end
   end
 
   def create
+  end
+
+  private
+
+  def album_params
+    params.require(:album).permit(
+      :id,
+      :user_id,
+      :title,
+      :artist,
+      :year,
+      :rank,
+      :art,
+      :notes,
+      :blurb,
+      :artists
+    )
   end
 end
--- react/src/components/SearchResultsList.js
+++ react/src/components/SearchResultsList.js
@@ -9,34 +9,38 @@ class SearchResultsList extends Component {
     );
   };
 
+  buttonClick(){
+    $.post({
+      url: '/albums/new.json',
+      type: "POST",
+      data: { id: id },
+      dataType: "json"
+    });
+  }
+
   renderAlbums(){
+    let onClick = () => this.buttonClick();
+    let addAlbum = <button className="button" onClick={onClick}>Add</button>
     if (this.props.albums === undefined) {
       return ''
     } else {
+      let totalAlbums = this.props.albums.albums.total;
+      let i = 0;
       return (
         <ul>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[0].images[2].url}/>
-          {this.props.albums.albums.items[0].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[1].images[2].url}/>
+          <div className="dropdown-item"><a href={"/albums/" + this.props.albums.albums.items[0].id}><img src={this.props.albums.albums.items[0].images[2].url}/>
+          {this.props.albums.albums.items[0].name}{addAlbum}</a></div>
+          <div className="dropdown-item"><a href="/albums/new" id={this.props.albums.albums.items[0].id}><img src={this.props.albums.albums.items[1].images[2].url}/>
           {this.props.albums.albums.items[1].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[2].images[2].url}/>
+          <div className="dropdown-item"><a href="/albums/new" id={this.props.albums.albums.items[0].id}><img src={this.props.albums.albums.items[2].images[2].url}/>
           {this.props.albums.albums.items[2].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[3].images[2].url}/>
+          <div className="dropdown-item"><a href="/albums/new" id={this.props.albums.albums.items[0].id}><img src={this.props.albums.albums.items[3].images[2].url}/>
           {this.props.albums.albums.items[3].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[4].images[2].url}/>
+          <div className="dropdown-item"><a href="/albums/new" id={this.props.albums.albums.items[0].id}><img src={this.props.albums.albums.items[4].images[2].url}/>
           {this.props.albums.albums.items[4].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[5].images[2].url}/>
+          <div className="dropdown-item"><a href="/albums/new" id={this.props.albums.albums.items[0].id}><img src={this.props.albums.albums.items[5].images[2].url}/>
           {this.props.albums.albums.items[5].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[6].images[2].url}/>
-          {this.props.albums.albums.items[6].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[7].images[2].url}/>
-          {this.props.albums.albums.items[7].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[8].images[2].url}/>
-          {this.props.albums.albums.items[8].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[9].images[2].url}/>
-          {this.props.albums.albums.items[9].name}</a></div>
-          <div className="dropdown-item"><a href="/albums/new"><img src={this.props.albums.albums.items[10].images[2].url}/>
-          {this.props.albums.albums.items[10].name}</a></div>
+          <div className="dropdown-item"><a href="/albums/new" id={this.props.albums.albums.items[0].id}>Or add it yourself</a></div>
         </ul>
       );
     };
