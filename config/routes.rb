0745f8314ca863b5409f1a2f848a6da5ed4c79d6
Michael Walsh <michael.c.walsh1@gmail.com>
2016-11-10 23:14:32 -0500

    added follower stats to user show page

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :albums, only: [:index]
   end
+  resources :users do
+    member do
+      get :following, :followers    
+    end
+  end
 end
