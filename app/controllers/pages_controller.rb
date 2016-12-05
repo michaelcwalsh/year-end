043033cc77ce6a23930a18d0c45c6ce68c6091c0
Michael Walsh <michael.c.walsh1@gmail.com>
2016-12-05 00:09:53 -0500

    moved some code from view to controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   def show
+    feed_items = []
+    current_user.following_ids.each do |id|
+      feed_items << Album.where(user_id: id)
+    end
+    @albums = []
+    feed_items.each do |following|
+      following.each do |album|
+        @albums << album
+      end
+    end
+    @albums.sort! { |a,b| b.created_at <=> a.created_at }
     render template: "pages/#{params[:page]}"
   end
 end
